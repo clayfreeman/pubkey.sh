56185fb68285361ab39afdd5b5fb0c8279d25fe0
Clay Freeman <git@clayfreeman.com>
2015-12-29 03:03:13 -0600

    add UNIQUE constraints to selector and token in models_token

--- schema/Token.sql
+++ schema/Token.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS models_token (
   id       INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
   uid      INTEGER  NOT NULL,
   expires  DATETIME NOT NULL,
-  selector TEXT     NOT NULL,
-  token    TEXT     NOT NULL,
+  selector TEXT     NOT NULL UNIQUE,
+  token    TEXT     NOT NULL UNIQUE,
 
   FOREIGN KEY(uid) REFERENCES models_user(id)
 );
