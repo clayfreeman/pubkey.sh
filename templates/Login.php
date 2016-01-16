102fc30d62d882020e6bf95b7985e45a904bd228
Clay Freeman <git@clayfreeman.com>
2016-01-15 21:07:22 -0600

    begin work on the login page

--- templates/Login.php
+++ templates/Login.php
@@ -1,7 +1,7 @@
 <div class="uk-vertical-align uk-text-center uk-height-1-1">
   <div class="uk-vertical-align-middle" style="width: 300px;">
     <a href="/">
-      <object type="image/svg+xml" data="/img/logo.svg" />
+      <object type="image/svg+xml" data="/img/logo.svg"></object>
     </a>
     <form class="uk-panel uk-panel-box uk-form">
       <div class="uk-form-row">
