901ae4d874ca2263e30bcb94898025aee8c04fc7
Clay Freeman <git@clayfreeman.com>
2016-01-15 21:04:13 -0600

    begin work on the login page

---
+++ Views/Login.php
@@ -0,0 +1,21 @@
+<?php
+  /**
+   * @file Login.php
+   *
+   * Defines functionality for the Login page
+   */
+
+  namespace Views;
+
+  class Login {
+    protected static $title = "Login";
+
+    public static function show() {
+      // Load and render the Login template
+      ob_start();
+      require(__PRIVATEROOT__.'/templates/Login.php');
+      $contents = ob_get_clean();
+      // Render and show the Page template
+      \Views\Page::show(self::$title, $contents);
+    }
+  }
--- public/index.php
+++ public/index.php
@@ -12,7 +12,12 @@
   $app = new \Slim\App;
 
   // Define route to homepage
-  $app->get('/', '\\Views\\Index::show');
+  $app->get ('/',      '\\Views\\Index::show');
+  // Define route to login form
+  $app->get ('/login', '\\Views\\Login::show');
+
+  // // Define route to login processing
+  // $app->post('/login', '\\Controllers\\Login::submit');
 
   // Run Slim app instance
   $app->run();
