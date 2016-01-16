1f83adc157ab21830557a585fdfdf5020c9fcf22
Clay Freeman <git@clayfreeman.com>
2016-01-15 20:48:12 -0600

    template common page elements

---
+++ Views/Page.php
@@ -0,0 +1,21 @@
+<?php
+  /**
+   * @file Page.php
+   *
+   * Defines common functionality for all pages
+   */
+
+  namespace Views;
+
+  class Page {
+    public static function show($title, $contents, $indent = 2) {
+      // Adjust the indent level of the contents
+      $indentText = str_repeat('  ', $indent);
+      $contents   = $indentText.implode("\n".$indentText, explode("\n",
+        $contents));
+      // Load and flush the Page template
+      ob_start();
+      require(__PRIVATEROOT__.'/templates/Page.php');
+      echo ob_get_clean();
+    }
+  }
---
+++ templates/Page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?= $title ?> | pubkey.sh</title>
+    <!-- Define page contents and character set -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- Load UIKit requirements -->
+    <link rel="stylesheet" href="/uikit/css/uikit.almost-flat.min.css" />
+    <script type="text/javascript" src="/uikit/js/uikit.min.js"></script>
+  </head>
+  <body>
+<?= $contents ?>
+  </body>
+</html>
