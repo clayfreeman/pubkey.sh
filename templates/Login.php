8a46acf778b8757968d2b22e8316c4438b072f18
Clay Freeman <git@clayfreeman.com>
2016-01-15 23:44:59 -0600

    begin work on the login page

--- templates/Login.php
+++ templates/Login.php
@@ -1,6 +1,6 @@
 <div class="uk-vertical-align uk-text-center uk-height-1-1">
   <div class="uk-vertical-align-middle" style="width: 300px;">
-    <a href="/"><object type="image/svg+xml" data="/img/logo.svg"></object></a>
+    <a href="/"><img src="/img/logo.svg" alt /></a>
     <form class="uk-panel uk-panel-box uk-form" method="POST">
       <div class="uk-form-row">
         <input class="uk-width-1-1 uk-form-large" name="username" placeholder="Username" type="text" />
