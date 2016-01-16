9288ba53950e944afe90b52478851039b8f7e74a
Clay Freeman <git@clayfreeman.com>
2016-01-16 00:49:51 -0600

    begin work on the login page

--- templates/Login.php
+++ templates/Login.php
@@ -2,7 +2,7 @@
   <div class="uk-vertical-align-middle" style="width: 300px;">
     <a href="/"><img class="uk-margin-bottom" src="/img/logo.svg" alt /></a>
     <form class="uk-panel uk-panel-box uk-form" method="POST">
-      <?php if (strlen(trim($error)) > 0) { ?><div class="uk-alert uk-alert-danger">
+      <?php if (strlen(trim($error)) > 0) { ?><div class="uk-alert uk-alert-danger" data-uk-alert>
         <a class="uk-alert-close uk-close" href></a>
         <?= htmlspecialchars(trim($error), ENT_HTML401 | ENT_QUOTES) ?>
       </div><?php } ?>
