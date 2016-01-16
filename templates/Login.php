d962757f31c707aa015ce94cebb6acac4587bf7c
Clay Freeman <git@clayfreeman.com>
2016-01-15 21:14:07 -0600

    begin work on the login page

--- templates/Login.php
+++ templates/Login.php
@@ -1,8 +1,8 @@
 <div class="uk-vertical-align uk-text-center uk-height-1-1">
   <div class="uk-vertical-align-middle" style="width: 300px;">
-    <a href="/">
+    <!-- <a href="/">
       <object type="image/svg+xml" data="/img/logo.svg"></object>
-    </a>
+    </a> -->
     <form class="uk-panel uk-panel-box uk-form">
       <div class="uk-form-row">
         <input class="uk-width-1-1 uk-form-large" name="username" placeholder="Username" type="text" />
@@ -11,7 +11,7 @@
         <input class="uk-width-1-1 uk-form-large" name="password" placeholder="Password" type="password" />
       </div>
       <div class="uk-form-row">
-        <a class="uk-width-1-1 uk-button uk-button-primary uk-button-large" href="#">Login</a>
+        <button class="uk-width-1-1 uk-button uk-button-primary uk-button-large">Submit</button>
       </div>
       <div class="uk-form-row uk-text-small">Lost Password?</div>
     </form>
