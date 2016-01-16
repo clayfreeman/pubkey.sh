8e5aa70d0d92a0c28a25ec0d32bc91f4ded6ba53
Clay Freeman <git@clayfreeman.com>
2016-01-15 23:00:17 -0600

    begin work on the login page

--- templates/Page.php
+++ templates/Page.php
@@ -5,6 +5,7 @@
     <!-- Define page contents and character set -->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <!-- Load jQuery -->
+    <script type="text/javascript" src="/js/jquery.min.js"></script>
     <!-- Load UIKit requirements -->
     <link rel="stylesheet" href="/uikit/css/uikit.almost-flat.min.css" />
     <script type="text/javascript" src="/uikit/js/uikit.min.js"></script>
