e7610cd465b2a3482071ed7e4099538b7d29c42e
Clay Freeman <git@clayfreeman.com>
2016-01-15 20:47:12 -0600

    template common page elements

--- Views/Index.php
+++ Views/Index.php
@@ -8,10 +8,14 @@
   namespace Views;
 
   class Index {
+    protected static $title = "Index";
+
     public static function show() {
-      // Load and flush the Index template
+      // Load and render the Index template
       ob_start();
       require(__PRIVATEROOT__.'/templates/Index.php');
-      ob_end_flush();
+      $contents = ob_get_clean();
+      // Render and show the Page template
+      \Views\Page::show(self::$title, $contents);
     }
   }
