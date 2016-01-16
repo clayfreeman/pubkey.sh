a207dcfd37123770e2dc852aa116d2e31d779a9f
Clay Freeman <git@clayfreeman.com>
2016-01-15 22:56:55 -0600

    begin work on the login page

--- templates/Page.php
+++ templates/Page.php
@@ -4,8 +4,10 @@
     <title><?= $title ?> | pubkey.sh</title>
     <!-- Define page contents and character set -->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- Load jQuery -->
     <!-- Load UIKit requirements -->
     <link rel="stylesheet" href="/uikit/css/uikit.almost-flat.min.css" />
+    <script type="text/javascript" src="/uikit/js/uikit.min.js"></script>
   </head>
   <body class="uk-height-1-1">
 <?= $contents ?>
