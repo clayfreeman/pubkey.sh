e1cb27aace3b6a760c3658bfd2c20b04fad58152
Clay Freeman <git@clayfreeman.com>
2016-01-18 16:34:20 -0600

    revamp error messages

--- Views/Message.php
+++ Views/Message.php
@@ -10,20 +10,19 @@
   class Message {
     public static function render($message, $type = null, $closable = true) {
       // Ensure the passed variable is a string
+      $output = null;
       if (is_string($message)) {
         // Optionally flag this alert as an error
-        $message  = indent('<div class="'.($type != null ? 'uk-alert-'.$type :
+        $output  = indent('<div class="'.($type != null ? 'uk-alert-'.$type :
           null).' uk-alert" data-uk-alert>');
         // If the message should be closable, print the close link
-        $message .= indent($closable ? '<a class="uk-alert-close uk-close" '.
+        $output .= indent($closable ? '<a class="uk-alert-close uk-close" '.
           'href></a>' : null, 1);
         // Encode the message in a paragraph
-        $message .= indent('<p>'.htmlspecialchars(trim($message), ENT_HTML401 |
+        $output .= indent('<p>'.htmlspecialchars(trim($output), ENT_HTML401 |
           ENT_QUOTES).'</p>', 1);
-        $message .= indent("</div>");
+        $output .= indent("</div>");
       }
-      else
-        $message = null;
-      return $message;
+      return $output;
     }
   }
