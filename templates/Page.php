ec1b3128851bcc30f31cf0b33d3ef08c921fcd1a
Clay Freeman <git@clayfreeman.com>
2016-01-18 16:14:06 -0600

    revamp error messages

--- templates/Page.php
+++ templates/Page.php
@@ -5,15 +5,19 @@
     <!-- Define page contents, character set, and viewport -->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+
     <!-- Load jQuery -->
     <script type="text/javascript" src="/js/jquery.min.js"></script>
+
     <!-- Load UIKit requirements -->
     <link rel="stylesheet" href="/uikit/css/uikit.almost-flat.min.css" />
     <link rel="stylesheet" href="/uikit/css/components/form-password.almost-flat.min.css" />
     <script type="text/javascript" src="/uikit/js/uikit.min.js"></script>
     <script type="text/javascript" src="/uikit/js/components/form-password.min.js"></script>
+
     <!-- Load UIKit CSS patches -->
     <link rel="stylesheet" href="/css/uikit.patches.css" />
+
     <!-- Load password strength verification library -->
     <script type="text/javascript" src="/js/zxcvbn.js"></script>
   </head>
