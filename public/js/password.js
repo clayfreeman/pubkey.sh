31ef420ca8c9331dc54f589bbd3c691d8f1f8454
Clay Freeman <git@clayfreeman.com>
2016-01-18 15:08:18 -0600

    add password strength verification to registration form

--- public/js/password.js
+++ public/js/password.js
@@ -7,8 +7,6 @@ $('input:password').change(function() {
       values.push(item);
     });
   });
-  // Log the restricted values to the console
-  console.log("Restricted values:" + JSON.stringify(values));
   // Fetch the information from zxcvbn regarding the password
   var info = zxcvbn($(this).val(), values);
   // If the score is sufficient, enable submission of the form
