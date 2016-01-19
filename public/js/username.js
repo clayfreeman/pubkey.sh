70fda617c8bca3cbc78344d57ef074b37d885f1e
Clay Freeman <git@clayfreeman.com>
2016-01-18 19:26:45 -0600

    revamp error messages

--- public/js/username.js
+++ public/js/username.js
@@ -7,44 +7,47 @@ function verifyUsername(selector) {
     var field  = $(this);
     var icon   = $(this).siblings('i');
     var form   = $(this).closest('form');
-    // Remove the icon's classes
-    icon.removeClass(function(index, css) {
-      return (css.match(/(^|\s)uk-icon-\S+/g) || []).join(' ');
-    });
-    // Change the state of the password field to show failure
-    field.attr('unverified', true);
-    field.removeClass('uk-form-success');
-    field.removeClass('uk-form-danger');
-    // Disable form submission
-    form.find('button').prop('disabled', true);
-    // Fetch the query result from the API
-    icon.addClass('uk-icon-refresh');
-    $.post('/user/available', {"username": $(this).val()}, function(data) {
+    // Verify constraints of the username field
+    if (field.val().length > 2 && /^[a-z][a-z0-9]{2,}$/i.test(field.val())) {
       // Remove the icon's classes
       icon.removeClass(function(index, css) {
         return (css.match(/(^|\s)uk-icon-\S+/g) || []).join(' ');
       });
-      available = $.parseJSON(data)['available'];
-      console.log(JSON.stringify(available));
-      if (available === true) {
-        // Change the state of the username field to show success
-        field.removeAttr('unverified');
-        field.removeClass('uk-form-danger');
-        field.addClass('uk-form-success');
-        icon.addClass('uk-icon-check');
-        // Enable form submission if appropriate
-        if (form.find('input[unverified]').length == 0)
-          form.find('button').prop('disabled', false);
-      }
-      else {
-        // Change the state of the password field to show failure
-        field.attr('unverified', true);
-        field.removeClass('uk-form-success');
-        field.addClass('uk-form-danger');
-        icon.addClass('uk-icon-close');
-        // Disable form submission
-        form.find('button').prop('disabled', true);
-      }
-    });
+      // Change the state of the password field to show failure
+      field.attr('unverified', true);
+      field.removeClass('uk-form-success');
+      field.removeClass('uk-form-danger');
+      // Disable form submission
+      form.find('button').prop('disabled', true);
+      // Fetch the query result from the API
+      icon.addClass('uk-icon-refresh');
+      $.post('/user/available', {"username": $(this).val()}, function(data) {
+        // Remove the icon's classes
+        icon.removeClass(function(index, css) {
+          return (css.match(/(^|\s)uk-icon-\S+/g) || []).join(' ');
+        });
+        available = $.parseJSON(data)['available'];
+        console.log(JSON.stringify(available));
+        if (available === true) {
+          // Change the state of the username field to show success
+          field.removeAttr('unverified');
+          field.removeClass('uk-form-danger');
+          field.addClass('uk-form-success');
+          icon.addClass('uk-icon-check');
+          // Enable form submission if appropriate
+          if (form.find('input[unverified]').length == 0)
+            form.find('button').prop('disabled', false);
+        }
+        else {
+          // Change the state of the password field to show failure
+          field.attr('unverified', true);
+          field.removeClass('uk-form-success');
+          field.addClass('uk-form-danger');
+          icon.addClass('uk-icon-close');
+          // Disable form submission
+          form.find('button').prop('disabled', true);
+        }
+      });
+    }
   });
 }
