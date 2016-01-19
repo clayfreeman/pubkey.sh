820e659d1677df062400c25d7f3935355b29a512
Clay Freeman <git@clayfreeman.com>
2016-01-18 18:50:37 -0600

    revamp error messages

--- schema/User.sql
+++ schema/User.sql
@@ -1,9 +1,9 @@
 DROP   TABLE IF     EXISTS models_user;
 CREATE TABLE IF NOT EXISTS models_user (
-  id    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  email TEXT    NOT NULL UNIQUE,
-  pass  TEXT    NOT NULL,
-  pid   INTEGER,
+  id   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  user TEXT    NOT NULL UNIQUE,
+  pass TEXT    NOT NULL,
+  pid  INTEGER,
 
   FOREIGN KEY(pid) REFERENCES models_pubkey(id)
 );
