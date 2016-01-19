94a2fa956ad767dc742c2ed27e0b1072e82c4fe3
Clay Freeman <git@clayfreeman.com>
2016-01-18 19:35:28 -0600

    revamp error messages

--- public/js/username.js
+++ public/js/username.js
@@ -1,3 +1,36 @@
+function usernameMutateState(field, enabled, indicateState = true) {
+  // Select the icon and form from the field
+  var icon = field.siblings('i');
+  var form = field.closest('form');
+  // Remove the icon's classes
+  icon.removeClass(function(index, css) {
+    return (css.match(/(^|\s)uk-icon-\S+/g) || []).join(' ');
+  });
+  // Change the state of the password field to show failure
+  if (enabled) {
+    field.removeAttr('unverified');
+    field.removeClass('uk-form-danger');
+    field.addClass('uk-form-success');
+    icon.addClass('uk-icon-check');
+    // Enable form submission if appropriate
+    if (form.find('input[unverified]').length == 0)
+      form.find('button').prop('disabled', false);
+  }
+  else {
+    field.attr('unverified', true);
+    field.removeClass('uk-form-success');
+    if (indicateState) {
+      field.addClass('uk-form-danger');
+      icon.addClass('uk-icon-close');
+    }
+    else {
+      field.removeClass('uk-form-danger');
+    }
+    // Disable form submission
+    form.find('button').prop('disabled', true);
+  }
+}
+
 function verifyUsername(selector) {
   // Add the unverified property to the password field
   $("input[name=" + selector + "]").attr('unverified', true);
@@ -7,56 +40,25 @@ function verifyUsername(selector) {
     var field  = $(this);
     var icon   = $(this).siblings('i');
     var form   = $(this).closest('form');
-    // Remove the icon's classes
-    icon.removeClass(function(index, css) {
-      return (css.match(/(^|\s)uk-icon-\S+/g) || []).join(' ');
-    });
     // Verify constraints of the username field
     if (field.val().length > 2 && /^[a-z][a-z0-9]{2,}$/i.test(field.val())) {
-      // Change the state of the password field to show failure
-      field.attr('unverified', true);
-      field.removeClass('uk-form-success');
-      field.removeClass('uk-form-danger');
-      // Disable form submission
-      form.find('button').prop('disabled', true);
+      // Disable the form field
+      usernameMutateState(field, false, false);
       // Fetch the query result from the API
       icon.addClass('uk-icon-refresh');
       $.post('/user/available', {"username": $(this).val()}, function(data) {
-        // Remove the icon's classes
-        icon.removeClass(function(index, css) {
-          return (css.match(/(^|\s)uk-icon-\S+/g) || []).join(' ');
-        });
         available = $.parseJSON(data)['available'];
-        console.log(JSON.stringify(available));
-        if (available === true) {
-          // Change the state of the username field to show success
-          field.removeAttr('unverified');
-          field.removeClass('uk-form-danger');
-          field.addClass('uk-form-success');
-          icon.addClass('uk-icon-check');
-          // Enable form submission if appropriate
-          if (form.find('input[unverified]').length == 0)
-            form.find('button').prop('disabled', false);
-        }
-        else {
-          // Change the state of the password field to show failure
-          field.attr('unverified', true);
-          field.removeClass('uk-form-success');
-          field.addClass('uk-form-danger');
-          icon.addClass('uk-icon-close');
-          // Disable form submission
-          form.find('button').prop('disabled', true);
-        }
+        if (available === true)
+          // Enable the form field
+          usernameMutateState(field, false, false);
+        else
+          // Disable the form field
+          usernameMutateState(field, false);
       });
     }
     else {
-      // Change the state of the password field to show failure
-      field.attr('unverified', true);
-      field.removeClass('uk-form-success');
-      field.addClass('uk-form-danger');
-      icon.addClass('uk-icon-close');
-      // Disable form submission
-      form.find('button').prop('disabled', true);
+      // Disable the form field
+      usernameMutateState(field, false);
     }
   });
 }
