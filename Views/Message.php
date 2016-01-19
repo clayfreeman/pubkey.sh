f46d5ab8877104f5cd3dfb1f1e56ac50503cd558
Clay Freeman <git@clayfreeman.com>
2016-01-18 18:32:14 -0600

    revamp error messages

--- Views/Message.php
+++ Views/Message.php
@@ -20,8 +20,8 @@
         $output  = indent('<div class="'.($type != null ? 'uk-alert-'.$type :
           null).' uk-alert" data-uk-alert>');
         // If the message should be closable, print the close link
-        $output .= indent($closable ? '<a class="uk-alert-close uk-close" '.
-          'href></a>' : null, 1);
+        $output .= $closable ? indent('<a class="uk-alert-close uk-close" '.
+          'href></a>', 1) : null;
         // Place the message in a paragraph
         $output .= indent('<p>'.$message.'</p>', 1);
         $output .= indent("</div>");
