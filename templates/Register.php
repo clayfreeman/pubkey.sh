441c290c8d37dd8cc852f0cafc87b299476d34ca
Clay Freeman <git@clayfreeman.com>
2016-01-18 19:09:36 -0600

    revamp error messages

--- templates/Register.php
+++ templates/Register.php
@@ -24,9 +24,9 @@
 </div>
 <!-- Ensure that this form's password is verified before submission -->
 <script type="text/javascript" src="/js/password.js">
-  verifyPassword('password');
+  $.ready(verifyPassword('password'));
 </script>
 <!-- Verify the availability of the username -->
 <script type="text/javascript" src="/js/username.js">
-  verifyUsername('username');
+  $.ready(verifyUsername('username'));
 </script>
