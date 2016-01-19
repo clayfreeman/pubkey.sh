79dda410e07743af68cc38033d8215b3c03d6659
Clay Freeman <git@clayfreeman.com>
2016-01-18 20:49:11 -0600

    revamp error messages

--- templates/Index.php
+++ templates/Index.php
@@ -1,5 +1,5 @@
-<?= indent($menu) ?><?= $message ?><div class="uk-container uk-container-center uk-margin-top">
-  <div class="uk-panel uk-panel-box">
+<?= indent($menu) ?><div class="uk-container uk-container-center uk-margin-top">
+  <?= indent($message, 1) ?><div class="uk-panel uk-panel-box">
     <h1>Hello World.</h1>
     <p>This is a test page.  Move along.</p>
   </div>
