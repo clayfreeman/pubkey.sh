c6ed8fdc222b157aa1ea6ae986423ef1da6944b9
Clay Freeman <git@clayfreeman.com>
2016-05-20 03:59:09 -0500

    modify utility scripts

--- app/launch.sh
+++ app/launch.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
 set -e
-
-# Set the permissions on the app directory
-chown -R root:www-data /app
-find /app -type d -exec chmod 550 '{}' \;
-find /app -type f -exec chmod 440 '{}' \;
 
 # Generate self-signed TLS certificate (if necessary)
 if [ ! -f "/data/server.key" ] || [ ! -f "/data/server.pem" ]; then
@@ -15,10 +9,28 @@ if [ ! -f "/data/server.key" ] || [ ! -f "/data/server.pem" ]; then
 fi
 
 # Set the appropriate permissions on the SSL key
-chown root:www-data /data/server.{key,pem}
-chmod 640           /data/server.{key,pem}
+chown root:www-data /data/server.key
+chown root:www-data /data/server.pem
+chmod 640           /data/server.key
+chmod 640           /data/server.pem
 
 # Create the database from its schema (if necessary)
 if [ ! -f "/data/pubkey.db" ]; then
   bash /app/schema/create.sh
+fi
+
+# Set the permissions on the app and data directories
+mkdir -p /app
+mkdir -p /data
+chown -R root:www-data /app
+find /app  -type d -exec chmod 550 '{}' \;
+find /app  -type f -exec chmod 440 '{}' \;
+chown -R root:www-data /data
+find /data -type d -exec chmod 770 '{}' \;
+find /data -type f -exec chmod 640 '{}' \;
+if [ -f "${DATASOURCE_PATH#sqlite:}" ]; then
+  chmod 660 "${DATASOURCE_PATH#sqlite:}"
+fi
+if [ -f "${HALITE_KEYFILE}" ]; then
+  chmod 660 "${HALITE_KEYFILE}"
 fi
--- app/schema/create.sh
+++ app/schema/create.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-mkdir -p /data
-echo "VACUUM;" | cat schema/*.sql - | sqlite3 /data/pubkey.db
-chown -R root:www-data /data
-chmod    750           /data
-chmod    660           /data/pubkey.db
+echo "VACUUM;" | cat /app/schema/*.sql - | sqlite3 /data/pubkey.db
+chmod 660 /data/pubkey.db
