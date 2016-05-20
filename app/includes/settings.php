8d0064c6047c97ce9c90b097d112c15ae3df7373
Clay Freeman <git@clayfreeman.com>
2016-05-20 01:46:47 -0500

    add settings.php that reads from getenv()

---
+++ app/includes/settings.php
@@ -0,0 +1,25 @@
+<?php
+  /**
+   * @file      settings.php
+   * @copyright Copyright 2016 Clay Freeman. All rights reserved
+   * @license   GNU General Public License v3 (GPL-3.0)
+   *
+   * This file serves as a non-public location to setup any requried settings
+   * used by the application
+   */
+
+  // Configure the database
+  \ORM::configure(getenv('DATASOURCE_PATH'));
+  if (strlen(trim(getenv('DATASOURCE_LOGIN'))) > 0)
+    \ORM::configure('username', getenv('DATASOURCE_LOGIN'));
+  if (strlen(trim(getenv('DATASOURCE_PASS'))) > 0)
+    \ORM::configure('password', getenv('DATASOURCE_PASS'));
+
+  // The time (in seconds) that it takes for a session to expire
+  define('__SESSIONEXPIRE__', intval(getenv('SESSION_EXPIRE')));
+
+  // The root domain of the application (used for cookie security restrictions)
+  define('__DOMAIN__', getenv('SERVER_NAME'));
+
+  // The encryption key file used for storing password information securely
+  define('__HALITEKEY__', getenv('HALITE_KEYFILE'));
