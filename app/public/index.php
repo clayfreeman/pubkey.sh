a6727ebccd8ecea50aeb42c7e4279ede562640dc
Clay Freeman <git@clayfreeman.com>
2016-05-26 01:06:14 -0500

    update the logout menu item

--- app/public/index.php
+++ app/public/index.php
@@ -68,7 +68,17 @@
       // The logout page should be restricted to logged in users from the menu
       'menu'    => ['anon' => false, 'user' => true],
       'methods' => [
-        'get'   => '\\Controllers\\User::logout'
+        'get'   => function(
+                     ServerRequestInterface $request,
+                     ResponseInterface      $response) {
+                       // Perform the user logout method
+                       \Controllers\User::logout();
+                       // Inform the user that they're logged out
+                       putSession('message', 'You have successfully logged '.
+                         'out.');
+                       // Redirect the user to the home page
+                       return $response->withRedirect('/');
+                     }
       ],
       'title'   => 'Logout'
     ],
